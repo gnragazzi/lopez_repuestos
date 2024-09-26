10104f8669b3d79e4743d0832003b143b4863aca
Gerardo Ragazzi <gnragazzi@gmail.com>
2024-09-26 10:20:04 -0300

    Layout básico de los componentes de la página principal

---
+++ cliente/src/componentes/Barra_superior.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Barra_superior() {
+  return (
+    <div className='barra_superior'>
+        <h1>Barra_superior</h1>
+    </div>
+  )
+}
+
+export default Barra_superior
--- cliente/src/componentes/Lateral.jsx
+++ cliente/src/componentes/Lateral.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function Lateral() {
+  const categorias = [
+    "principal",
+    "viajes",
+    "empleados",
+    "vehículos",
+    "mantenimiento"
+  ]
   return (
-    <div className='lateral'>Lateral</div>
+    <div className='lateral'>
+      <ul>
+        {categorias.map((categoría)=>{
+          return <li key={categoría}><p>{categoría}</p></li>
+        })}
+      </ul>
+    </div>
   )
 }
 
