a418e1757127f0ee961000d5d3b73c0002334527
Ezequiel <ezequielnodarhbk@gmail.com>
2024-09-28 22:27:33 -0300

    hice algo del lateral

--- cliente/src/assets/data.jsx
+++ cliente/src/assets/data.jsx
@@ -1,22 +1,33 @@
+import icono_principal from "../assets/iconos_lateral/icono_principal.svg"
+import icono_viaje from "../assets/iconos_lateral/icono_viaje.svg"
+import icono_empleado from "../assets/iconos_lateral/icono_empleado.svg"
+import icono_vehiculos from "../assets/iconos_lateral/icono_vehiculos.svg"
+import icono_mantenimiento from "../assets/iconos_lateral/icono_mantenimiento.svg"
+
 export const categorias = [
     {
       nombre: "principal",
+      img: icono_principal,
       subcategorias: null
     },
     {
       nombre: "viajes",
+      img: icono_viaje,
       subcategorias: null
     },
     {
       nombre: "empleados",
+      img: icono_empleado,
       subcategorias: ["choferes","mecánicos"]
     },
     {
       nombre: "vehículos",
+      img: icono_vehiculos,
       subcategorias: ["camiones","semi-remolques"]
     },
     {
       nombre: "mantenimiento",
+      img: icono_mantenimiento,
       subcategorias: null
     },
   ]
--- cliente/src/componentes/Lateral.jsx
+++ cliente/src/componentes/Lateral.jsx
@@ -1,50 +1,61 @@
 import { categorias } from "../assets/data";
 import { useContextoGlobal } from "../Contexto";
 import { Link } from "react-router-dom";
+import arrow from "../assets/iconos_lateral/arrow.svg";
+import icono_lopez from "../assets/iconos_lopez/icono_lopez.png";
 
 function Lateral() {
   const { clickMenu, menuSeleccionado } = useContextoGlobal();
-
   return (
     <div className="lateral">
+      <div className="icono_lopez_lateral">
+        <img src={icono_lopez} />
+      </div>
       <ul>
-        {categorias.map(({ nombre, subcategorias }) => {
+        {categorias.map(({ nombre, subcategorias, img }) => {
           return subcategorias ? (
             <div key={nombre}>
+                <li
+                  className={
+                    menuSeleccionado == nombre ? "seleccion" : "no_seleccion"
+                  }
+                  onClick={clickMenu}
+                >
+                  <img className="icono_lateral" src={img} />
+                  <p>{nombre}</p>
+                  
+                  <img
+                  className={menuSeleccionado == nombre ? "arrow open" : "arrow"}
+                  src={arrow}/>
+
+                </li>
+              {subcategorias.map((sub) => {
+                return (
+                    <li
+                      key={sub}
+                      className={
+                        menuSeleccionado == nombre ? "seleccion_visible": "oculto"
+                      }
+                      onClick={()=> console.log("jasjasj")}
+                    >
+                      <a to={`/${nombre}`}><p>{sub}</p></a>
+                    </li>
+                );
+              })}
+            </div>
+          ) : (
+            <Link to={`/${nombre}`}>
               <li
+                key={nombre}
                 className={
                   menuSeleccionado == nombre ? "seleccion" : "no_seleccion"
                 }
                 onClick={clickMenu}
               >
+                <img src={img} />
                 <p>{nombre}</p>
               </li>
-              {subcategorias.map((sub) => {
-                return (
-                  <li
-                    key={sub}
-                    className={
-                      menuSeleccionado == nombre
-                        ? "seleccion visible"
-                        : " oculto"
-                    }
-                    onClick={clickMenu}
-                  >
-                    <p>{sub}</p>
-                  </li>
-                );
-              })}
-            </div>
-          ) : (
-            <li
-              key={nombre}
-              className={
-                menuSeleccionado == nombre ? "seleccion" : "no_seleccion"
-              }
-              onClick={clickMenu}
-            >
-              <Link to={`/${nombre}`}>{nombre}</Link>
-            </li>
+            </Link>
           );
         })}
       </ul>
