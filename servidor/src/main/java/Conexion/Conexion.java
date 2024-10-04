34cb0341926a67ab224448860c5fec4bd831ab21
Ezequiel <ezequielnodarhbk@gmail.com>
2024-10-04 14:23:51 -0300

    merge con is14

---
+++ servidor/src/main/java/Conexion/Conexion.java
@@ -0,0 +1,58 @@
+package Conexion;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private static Conexion instanciaUnica;
+
+    private Connection conexion;
+
+    //A la hora de querer hacer una consulta a la base se tiene que hacer: 
+    //Connection conexion = Conexion.getInstancia().getConexion();
+    //A la hora de cerrar la conexion se hace:
+    //Conexion.getInstancia().cerrarConexion();
+    private Conexion() throws ClassNotFoundException {
+        String url = Credenciales.getUrl();
+        String usuario = Credenciales.getUsuario();
+        String  contraseña= Credenciales.getContraseña();
+        
+
+        try {
+            Class.forName(Credenciales.getDRIVERS());
+            conexion = DriverManager.getConnection(url, usuario, contraseña);
+            System.out.println("Connection is successful");
+            System.out.println(conexion);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Conexion getInstancia() throws ClassNotFoundException {
+        if (instanciaUnica == null) {
+            instanciaUnica = new Conexion();
+        }
+        return instanciaUnica;
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void closeConexion() {
+        if (conexion != null) {
+            try {
+                conexion.close();
+                System.out.println("Conexión cerrada.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- servidor/src/main/java/com/grupoing/servidor/Servidor.java
+++ servidor/src/main/java/com/grupoing/servidor/Servidor.java
@@ -4,20 +4,35 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
+import Conexion.Conexion;
+import com.grupoing.servidor.manejadorMantenimiento;
+import com.grupoing.servidor.manejadorMecanicos;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+import com.grupoing.servidor.manejadorVehiculos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Servidor {
 
     public static int port = 8080;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         System.out.println("Servidor Montado en el puerto " + port);
         server.createContext("/vehiculos", new manejadorVehiculos());
         server.createContext("/mecanicos", new manejadorMecanicos());
         server.createContext("/mantenimiento", new manejadorMantenimiento());
         server.createContext("/cargar_viaje", new manejadorViaje());
-
+        
+        
         server.setExecutor(null);
+        Connection conexion = Conexion.getInstancia().getConexion();
+        String sql = "insert into tarjetas_ruta (Fecha_Emision, Fecha_Vencimiento) value ('2024-03-01','2024-03-02');";
+        PreparedStatement envio;
+            envio = conexion.prepareStatement("insert into tarjetas_ruta (Fecha_Emision, Fecha_Vencimiento) value ('2024-04-01','2024-05-02');");
+            envio.executeUpdate();  
         server.start();
     }
 }
