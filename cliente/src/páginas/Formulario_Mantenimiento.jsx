65200be6f04b17f17d8b91b51aa0114a6b3b8a8a
Gerardo Ragazzi <gnragazzi@gmail.com>
2024-10-08 20:54:38 -0300

    Corregí omisión de campo en cliente

--- cliente/src/páginas/Formulario_Mantenimiento.jsx
+++ cliente/src/páginas/Formulario_Mantenimiento.jsx
@@ -2,11 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-
-
-
-
-
 export const Formulario_Mantenimiento = () => {
   const navegar = useNavigate();
   const [pantalla, setPantalla] = useState(0);
@@ -20,16 +15,23 @@ export const Formulario_Mantenimiento = () => {
     trabajos_realizados: "",
     costo_repuestos: 0,
     costo_manodeobra: 0,
+    kilometros_en_que_se_realizo: 0,
     fecha: new Date().toISOString().substring(0, 10),
   });
   const enviarFormulario = () => {
-    const { trabajos_realizados, costo_manodeobra, costo_repuestos, fecha } =
-      datos;
+    const {
+      trabajos_realizados,
+      costo_manodeobra,
+      costo_repuestos,
+      fecha,
+      kilometros_en_que_se_realizo,
+    } = datos;
     const cuerpo = {
       trabajos_realizados,
       fecha,
       costo_repuestos,
       costo_manodeobra,
+      kilometros_en_que_se_realizo,
       vehiculo: {
         vehiculoSeleccionado,
         esCamion,
@@ -82,36 +84,38 @@ export const Formulario_Mantenimiento = () => {
                 </tr>
               </thead>
               <tbody>
-              {vehiculos.map((vehiculo) => {
-              const { patente, marca } = vehiculo;
-              return (
-              <tr className={
-                vehiculoSeleccionado == patente
-                  ? "vehiculos_lista vehiculos_lista_seleccionado"
-                  : "vehiculos_lista"
-              }
-              key={patente}
-              onClick={() => {
-                patente != vehiculoSeleccionado
-                  ? setVehiculoSeleccionado(patente)
-                  : setVehiculoSeleccionado("");
-                //verificamos un atributo que solo un camión tendría para determinar si es camión o semiremolque
-                vehiculo.cilindrada
-                  ? setEsCamion(true)
-                  : setEsCamion(false);
-              }}>
-                <td>
-                  <p>{marca}</p>
-                </td>
-                <td>
-                  <p>{patente}</p>
-                </td>
-              </tr>
-              );
-            })}
+                {vehiculos.map((vehiculo) => {
+                  const { patente, marca } = vehiculo;
+                  return (
+                    <tr
+                      className={
+                        vehiculoSeleccionado == patente
+                          ? "vehiculos_lista vehiculos_lista_seleccionado"
+                          : "vehiculos_lista"
+                      }
+                      key={patente}
+                      onClick={() => {
+                        patente != vehiculoSeleccionado
+                          ? setVehiculoSeleccionado(patente)
+                          : setVehiculoSeleccionado("");
+                        //verificamos un atributo que solo un camión tendría para determinar si es camión o semiremolque
+                        vehiculo.potencia
+                          ? setEsCamion(true)
+                          : setEsCamion(false);
+                      }}
+                    >
+                      <td>
+                        <p>{marca}</p>
+                      </td>
+                      <td>
+                        <p>{patente}</p>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
-        </table>
-      </div>
+            </table>
+          </div>
         </>
       )}
       {pantalla == 1 && (
@@ -154,17 +158,17 @@ export const Formulario_Mantenimiento = () => {
                       </td>
                       <td>
                         <p>{nombre}</p>
-                      </td> 
+                      </td>
                       <td>
                         <p>{apellido}</p>
                       </td>
                     </tr>
                   );
                 })}
-          </tbody>
-        </table>
-      </div>
-      </>
+              </tbody>
+            </table>
+          </div>
+        </>
       )}
       {pantalla == 2 && (
         <>
@@ -182,7 +186,7 @@ export const Formulario_Mantenimiento = () => {
               />
             </fieldset>
             <fieldset className="form__items-mantenimiento">
-              <legend  className="form__legend">Costo de Repuesto</legend>
+              <legend className="form__legend">Costo de Repuesto</legend>
               <input
                 className="items__input"
                 placeholder="Costo de Repuesto"
@@ -194,7 +198,7 @@ export const Formulario_Mantenimiento = () => {
               />
             </fieldset>
             <fieldset className="form__items-mantenimiento">
-              <legend  className="form__legend">Costo de Mano de Obra</legend>
+              <legend className="form__legend">Costo de Mano de Obra</legend>
               <input
                 className="items__input"
                 placeholder="Costo de Mano de Obra"
@@ -205,8 +209,29 @@ export const Formulario_Mantenimiento = () => {
                 }
               />
             </fieldset>
+            {/* // nuevo campo */}
+            {esCamion && (
+              <fieldset className="form__items-mantenimiento">
+                <legend className="form__legend">
+                  Kilometros del camión en que se realizó el mantenimiento
+                </legend>
+                <input
+                  className="items__input"
+                  placeholder="Costo de Mano de Obra"
+                  type="number"
+                  value={datos.kilometros_en_que_se_realizo}
+                  onChange={(e) =>
+                    setDatos({
+                      ...datos,
+                      kilometros_en_que_se_realizo: e.target.value,
+                    })
+                  }
+                />
+              </fieldset>
+            )}
+
             <fieldset className="form__items-mantenimiento">
-              <legend  className="form__legend">Fecha</legend>
+              <legend className="form__legend">Fecha</legend>
               <input
                 className="items__input"
                 type="date"
@@ -222,44 +247,53 @@ export const Formulario_Mantenimiento = () => {
       {pantalla == 3 && (
         <>
           <h2>Confirme Selección</h2>
-            <div className="confirmar__seleccion">
-              {vehiculos.map((v) => {
-                return (
-                  v.patente == vehiculoSeleccionado && (
-                    <p key={v.patente}>
-                      Marca: {v.marca} | Patente {v.patente}
-                    </p>
-                  )
-                );
-              })}
-              {mecanicos.map((m) => {
-                return (
-                  mecanicosSeleccionados.includes(m.dni) && (
-                    <p key={m.dni}>
-                      Nombre: {m.nombre} {m.apellido} | DNI: {m.dni}
-                    </p>
-                  )
-                );
-              })}
-          <p>Trabajo Realizado: {datos.trabajos_realizados}</p>
-          <p>Costo Repuesto: {datos.costo_repuestos}</p>
-          <p>Costo Mano de Obra: {datos.costo_manodeobra}</p>
-          <p>Fecha: {datos.fecha}</p>
+          <div className="confirmar__seleccion">
+            {vehiculos.map((v) => {
+              return (
+                v.patente == vehiculoSeleccionado && (
+                  <p key={v.patente}>
+                    Marca: {v.marca} | Patente {v.patente}
+                  </p>
+                )
+              );
+            })}
+            {mecanicos.map((m) => {
+              return (
+                mecanicosSeleccionados.includes(m.dni) && (
+                  <p key={m.dni}>
+                    Nombre: {m.nombre} {m.apellido} | DNI: {m.dni}
+                  </p>
+                )
+              );
+            })}
+            <p>Trabajo Realizado: {datos.trabajos_realizados}</p>
+            <p>Costo Repuesto: {datos.costo_repuestos}</p>
+            <p>Costo Mano de Obra: {datos.costo_manodeobra}</p>
+            <p>Fecha: {datos.fecha}</p>
           </div>
         </>
       )}
 
       <div className="botonera_formulario">
         {pantalla == 0 && (
-          <button className="formulario__boton volver" onClick={() => navegar("/mantenimiento")}>
+          <button
+            className="formulario__boton volver"
+            onClick={() => navegar("/mantenimiento")}
+          >
             Volver
           </button>
         )}
         {pantalla > 0 && (
-          <button className="formulario__boton volver" onClick={() => setPantalla(pantalla - 1)}>Volver</button>
+          <button
+            className="formulario__boton volver"
+            onClick={() => setPantalla(pantalla - 1)}
+          >
+            Volver
+          </button>
         )}
         {pantalla < 3 && (
-          <button className="formulario__boton siguiente" 
+          <button
+            className="formulario__boton siguiente"
             onClick={() => {
               if (
                 (pantalla == 0 && vehiculoSeleccionado) ||
@@ -276,7 +310,14 @@ export const Formulario_Mantenimiento = () => {
             Siguiente
           </button>
         )}
-        {pantalla == 3 && <button className="formulario__boton siguiente" onClick={enviarFormulario}>Confirmar</button>}
+        {pantalla == 3 && (
+          <button
+            className="formulario__boton siguiente"
+            onClick={enviarFormulario}
+          >
+            Confirmar
+          </button>
+        )}
       </div>
     </div>
   );
