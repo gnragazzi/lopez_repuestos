bb64cea4f8030b65a54a85a6823b95585f940a2a
EzeBernaldez <ezeebernaldez@gmail.com>
2024-10-08 23:35:41 -0300

    hice front de viajes

--- cliente/src/páginas/cargar_viaje/Cargar_viaje_4.jsx
+++ cliente/src/páginas/cargar_viaje/Cargar_viaje_4.jsx
@@ -30,58 +30,69 @@ const Cargar_viaje_3 = ({ dispatch, acciones, estado }) => {
       .catch((error) => console.log(error));
   }, []);
   return (
-    <div>
+    <div className="App formulario">
       <h2>Selección de Chofer</h2>
-      <h4>Seleccione el Chofer para el viaje</h4>
-      <ul>
-        <li className="vehiculos_lista header_lista">
-          <p>Marca</p>
-          <p>DNI</p>
-        </li>
-        {lista_choferes.map((chofer) => {
-          const { dni, marca } = chofer;
-          return (
-            <li
-              className={
-                filtro_chofer.includes(dni)
-                  ? "vehiculos_lista no_disponible"
-                  : chofer_seleccionado == dni
-                  ? "vehiculos_lista vehiculos_lista_seleccionado"
-                  : "vehiculos_lista"
-              }
-              key={dni}
-              onClick={
-                filtro_chofer.includes(dni)
-                  ? () => {
-                      console.log("No se puede seleccionar");
-                    }
-                  : () => {
-                      dispatch({
-                        type: SELECCIONAR_CHOFER,
-                        payload: dni,
-                      });
-                    }
-              }
-            >
-              <p>{marca}</p>
-              <p>{dni}</p>
-            </li>
-          );
-        })}
-      </ul>
-      <button onClick={() => dispatch({ type: PANTALLA_ANTERIOR })}>
-        Pantalla anterior
-      </button>
-      <button
-        onClick={() =>
-          dispatch({
-            type: PROXIMA_PANTALLA,
-            payload: [Boolean(chofer_seleccionado)],
-          })
-        }
-      >
-        siguiente
-      </button>
+      <br />
+      <div className="container__table form-table">
+        <table className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>DNI</th>
+                    <th>Apellido</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {lista_choferes.map((chofer) => {
+                    const { dni, marca } = chofer;
+                    return (
+                      <tr
+                        className={
+                          filtro_chofer.includes(dni)
+                            ? "vehiculos_lista no_disponible"
+                            : chofer_seleccionado == dni
+                            ? "vehiculos_lista vehiculos_lista_seleccionado"
+                            : "vehiculos_lista"
+                        }
+                        key={dni}
+                        onClick={
+                          filtro_chofer.includes(dni)
+                            ? () => {
+                                console.log("No se puede seleccionar");
+                              }
+                            : () => {
+                                dispatch({
+                                  type: SELECCIONAR_CHOFER,
+                                  payload: dni,
+                                });
+                              }
+                        }
+                      >
+                        <td><p>{marca}</p></td>
+                        <td><p>{dni}</p></td>
+
+                      </tr>
+                    );
+                  })}
+                </tbody>
+        </table>
+      </div>
+
+      <div className="botonera_formulario">
+        <button className="formulario__boton volver" onClick={() => dispatch({ type: PANTALLA_ANTERIOR })}>
+          Volver
+        </button>
+        <button className="formulario__boton siguiente"
+          onClick={() =>
+            dispatch({
+              type: PROXIMA_PANTALLA,
+              payload: [Boolean(chofer_seleccionado)],
+            })
+          }
+        >
+          Siguiente
+        </button>
+      </div>
+
     </div>
   );
 };
