edaf9f5362e872fc7057c34c774f9819bded2462
EzeBernaldez <ezeebernaldez@gmail.com>
2024-10-09 02:16:14 -0300

    coloque algunas transiciones y gradientes. Tmb los iconos

--- cliente/src/componentes/Barra_superior.jsx
+++ cliente/src/componentes/Barra_superior.jsx
@@ -1,4 +1,8 @@
 import icono_lopez from "../assets/iconos_lopez/icono_empresa.png";
+import { IoSettingsSharp } from "react-icons/io5";
+import { MdContactSupport } from "react-icons/md";
+import { IoMdContact } from "react-icons/io";
+import { IoIosNotifications } from "react-icons/io";
 
 
 
@@ -8,7 +12,13 @@ function Barra_superior() {
       <div className="barra_superior__icono_superior">
         <img src={icono_lopez} alt="Icono de la empresa" />
       </div>
-      <h1>Barra_superior</h1>
+      <div className="nav__barra_superior">
+        <a className="iconos_barra_superior" to="#"> <IoSettingsSharp className="icon"/></a>
+        <a className="iconos_barra_superior" to="#"><MdContactSupport className="icon"/></a>
+        <a className="iconos_barra_superior" to="#"><IoMdContact className="icon"/></a>
+        <a className="iconos_barra_superior" to="#"><IoIosNotifications className="icon"/></a>
+      </div>
+
     </div>
   );
 }
