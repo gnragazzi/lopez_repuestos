7b84bfc8f56a918266495de46518693158bfdc04
Ezequiel <ezequielnodarhbk@gmail.com>
2024-10-11 14:15:49 -0300

    hice que los mantenimientos cargados se muestren en pantalla

--- cliente/src/páginas/Mantenimiento.jsx
+++ cliente/src/páginas/Mantenimiento.jsx
@@ -1,86 +1,21 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Modal, ModalBody, ModalHeader, ModalFooter } from "reactstrap";
+import axios from "axios";
 
 function Mantenimiento() {
-  const dataPaises = [
-    { id: 1, nombre: "Filipinas", minutos: 241 },
-    { id: 2, nombre: "Brasil", minutos: 225 },
-    { id: 3, nombre: "Colombia", minutos: 216 },
-    { id: 4, nombre: "Nigeria", minutos: 216 },
-    { id: 5, nombre: "Argentina", minutos: 207 },
-    { id: 6, nombre: "Indonesia", minutos: 195 },
-    { id: 7, nombre: "Emiratos Árabes Unidos", minutos: 191 },
-    { id: 8, nombre: "México", minutos: 190 },
-    { id: 9, nombre: "Sudáfrica", minutos: 190 },
-    { id: 10, nombre: "Egipto", minutos: 186 },
-    { id: 11, nombre: "Filipinas", minutos: 241 },
-    { id: 12, nombre: "Brasil", minutos: 225 },
-    { id: 13, nombre: "Colombia", minutos: 216 },
-    { id: 14, nombre: "Nigeria", minutos: 216 },
-    { id: 15, nombre: "Argentina", minutos: 207 },
-    { id: 16, nombre: "Indonesia", minutos: 195 },
-    { id: 17, nombre: "Emiratos Árabes Unidos", minutos: 191 },
-    { id: 18, nombre: "México", minutos: 190 },
-    { id: 19, nombre: "Sudáfrica", minutos: 190 },
-    { id: 20, nombre: "Egipto", minutos: 186 },
-  ];
+  
+  const [mantenimientos, setMantenimientos] = useState([]);
 
-  const [data, setData] = useState(dataPaises);
-  const [modalEditar, setModalEditar] = useState(false);
-  const [modalEliminar, setModalEliminar] = useState(false);
-  const [modalInsertar, setModalInsertar] = useState(false);
-
-  const [paisSeleccionado, setPaisSeleccionado] = useState({
-    id: "",
-    nombre: "",
-    minutos: "",
-  });
-
-  const seleccionarPais = (elemento, caso) => {
-    setPaisSeleccionado(elemento);
-    caso === "Editar" ? setModalEditar(true) : setModalEliminar(true);
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPaisSeleccionado((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const editar = () => {
-    var dataNueva = data;
-    dataNueva.map((pais) => {
-      if (pais.id === paisSeleccionado.id) {
-        pais.minutos = paisSeleccionado.minutos;
-        pais.nombre = paisSeleccionado.nombre;
-      }
-    });
-    setData(dataNueva);
-    setModalEditar(false);
-  };
-
-  const eliminar = () => {
-    setData(data.filter((pais) => pais.id !== paisSeleccionado.id));
-    setModalEliminar(false);
-  };
-
-  // const abrirModalInsertar = () => {
-  //   setPaisSeleccionado(null);
-  //   setModalInsertar(true);
-  // };
-
-  const insertar = () => {
-    var valorInsertar = paisSeleccionado;
-    valorInsertar.id = data[data.length - 1].id + 1;
-    var dataNueva = data;
-    dataNueva.push(valorInsertar);
-    setData(dataNueva);
-    setModalInsertar(false);
-  };
+  useEffect(() => {
+    axios.get('http://localhost:8080/mantenimiento')
+      .then((response) => {
+        setMantenimientos(response.data);
+      })
+      .catch((error) => {
+        console.error("error");
+      });
+  }, []);
 
   return (
     <div className="App">
@@ -97,31 +32,36 @@ function Mantenimiento() {
         <table className="table table-bordered">
           <thead>
             <tr>
-              <th>ID</th>
+              <th>Trabajos realizados</th>
+              <th>Patente del vehiculo</th>
+              <th>Marca</th>
               <th>Fecha</th>
-              <th>Vehiculo</th>
-              <th>Costo total</th>
+              <th>Costo de mano de obra</th>
+              <th>Costo de repuestos</th>
               <th className="td__botones">Acciones</th>
             </tr>
           </thead>
           <tbody>
-            {data.map((elemento) => (
-              <tr key={elemento.id}>
-                <td>{elemento.id}</td>
-                <td>{elemento.nombre}</td>
-                <td>{elemento.minutos}</td>
-                <td>gfd</td>
+            {mantenimientos.map((elemento, index) => (
+              <tr key={index}>  {/* Aquí podrías usar un ID único en lugar de index */}
+                <td>{elemento.trabajos_realizados}</td>
+                <td>{elemento.vehiculo.patente}</td>
+                <td>{elemento.vehiculo.marca}</td>
+                {/* Formateo de fecha */}
+                <td>{elemento.fecha[2]}/{elemento.fecha[1]}/{elemento.fecha[0]}</td>
+                <td>${elemento.costos_manodeobra}</td>
+                <td>${elemento.costos_repuestos}</td>
                 <td className="td__botones tr-boton">
                   <button
                     className="btn btn-primary"
-                    onClick={() => seleccionarPais(elemento, "Editar")}
+                    onClick={() => console.log("Editar:", elemento)}
                   >
                     Editar
-                  </button>{" "}
+                  </button>
                   {"   "}
                   <button
                     className="btn btn-danger"
-                    onClick={() => seleccionarPais(elemento, "Eliminar")}
+                    onClick={() => console.log("Eliminar:", elemento)}
                   >
                     Eliminar
                   </button>
@@ -131,127 +71,6 @@ function Mantenimiento() {
           </tbody>
         </table>
       </div>
-      <Modal isOpen={modalEditar}>
-        <ModalHeader>
-          <div>
-            <h3>Editar País</h3>
-          </div>
-        </ModalHeader>
-        <ModalBody>
-          <div className="form-group">
-            <label>ID</label>
-            <input
-              className="form-control"
-              readOnly
-              type="text"
-              name="id"
-              value={paisSeleccionado && paisSeleccionado.id}
-            />
-            <br />
-
-            <label>País</label>
-            <input
-              className="form-control"
-              type="text"
-              name="nombre"
-              value={paisSeleccionado && paisSeleccionado.nombre}
-              onChange={handleChange}
-            />
-            <br />
-
-            <label>Minutos</label>
-            <input
-              className="form-control"
-              type="text"
-              name="minutos"
-              value={paisSeleccionado && paisSeleccionado.minutos}
-              onChange={handleChange}
-            />
-            <br />
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-primary" onClick={() => editar()}>
-            Actualizar
-          </button>
-          <button
-            className="btn btn-danger"
-            onClick={() => setModalEditar(false)}
-          >
-            Cancelar
-          </button>
-        </ModalFooter>
-      </Modal>
-
-      <Modal isOpen={modalEliminar}>
-        <ModalBody>
-          Estás Seguro que deseas eliminar el país{" "}
-          {paisSeleccionado && paisSeleccionado.nombre}
-        </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-danger" onClick={() => eliminar()}>
-            Sí
-          </button>
-          <button
-            className="btn btn-secondary"
-            onClick={() => setModalEliminar(false)}
-          >
-            No
-          </button>
-        </ModalFooter>
-      </Modal>
-
-      <Modal isOpen={modalInsertar}>
-        <ModalHeader>
-          <div>
-            <h3>Insertar País</h3>
-          </div>
-        </ModalHeader>
-        <ModalBody>
-          <div className="form-group">
-            <label>ID</label>
-            <input
-              className="form-control"
-              readOnly
-              type="text"
-              name="id"
-              value={data[data.length - 1].id + 1}
-            />
-            <br />
-
-            <label>País</label>
-            <input
-              className="form-control"
-              type="text"
-              name="nombre"
-              value={paisSeleccionado ? paisSeleccionado.nombre : ""}
-              onChange={handleChange}
-            />
-            <br />
-
-            <label>Minutos</label>
-            <input
-              className="form-control"
-              type="text"
-              name="minutos"
-              value={paisSeleccionado ? paisSeleccionado.minutos : ""}
-              onChange={handleChange}
-            />
-            <br />
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-primary" onClick={() => insertar()}>
-            Insertar
-          </button>
-          <button
-            className="btn btn-danger"
-            onClick={() => setModalInsertar(false)}
-          >
-            Cancelar
-          </button>
-        </ModalFooter>
-      </Modal>
     </div>
   );
 }
