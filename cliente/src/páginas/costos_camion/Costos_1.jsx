275fc53d1501fe26dc377ea0f1e9a9daa8080dc8
EzeBernaldez <ezeebernaldez@gmail.com>
2024-10-12 19:51:10 -0300

    arreglamos costos y pusimos el titilo a los botones

--- cliente/src/páginas/costos_camion/Costos_1.jsx
+++ cliente/src/páginas/costos_camion/Costos_1.jsx
@@ -24,50 +24,57 @@ const Costos_1 = () => {
   return (
     <>
       <h2>Seleccione la Fecha:</h2>
-      <div>
-        <form>
-          <label htmlFor="nes">Mes:</label>
-          <select
-            id="mes"
-            name="mes"
-            value={meses[estado.mes_costo]}
-            onChange={(e) => {
-              dispatch({
-                type: SELECCIONAR_MES,
-                payload: meses.indexOf(e.target.value),
-              });
-            }}
-          >
-            {meses.map((mes, i) => {
-              return (
-                <option key={i} value={mes}>
-                  {mes}
-                </option>
-              );
-            })}
-          </select>
-          <label htmlFor="año">Año: </label>
-          <select
-            id="año"
-            name="año"
-            value={estado.año_costo}
-            onChange={(e) => {
-              dispatch({
-                type: SELECCIONAR_AÑO,
-                payload: e.target.value,
-              });
-            }}
-          >
-            {años.map((año) => {
-              return (
-                <option key={año} value={año}>
-                  {año}
-                </option>
-              );
-            })}
-          </select>
+        <form form className="form-table form__mantenimiento">
+            <fieldset className="form__items-mantenimiento">
+              <legend className="form__legend">
+                Mes
+              </legend>
+              <select className="items__input"
+                id="mes"
+                name="mes"
+                value={meses[estado.mes_costo]}
+                onChange={(e) => {
+                  dispatch({
+                    type: SELECCIONAR_MES,
+                    payload:  meses.indexOf(e.target.value),
+                  });
+                }}
+              >
+                {meses.map((mes,i) => {
+                  return (
+                    <option key={i} value={mes}>
+                      {mes}
+                    </option>
+                  );
+                })}
+              </select>
+            </fieldset> 
+
+            <fieldset className="form__items-mantenimiento">
+              <legend className="form__legend">
+                Año
+              </legend>
+              <select className="items__input"
+                id="año"
+                name="año"
+                value={estado.año_costo}
+                onChange={(e) => {
+                  dispatch({
+                    type: SELECCIONAR_AÑO,
+                    payload: e.target.value,
+                  });
+                }}
+              >
+                {años.map((año) => {
+                  return (
+                    <option key={año} value={año}>
+                      {año}
+                    </option>
+                  );
+                })}
+              </select>
+            </fieldset>          
         </form>
-      </div>
     </>
   );
 };
