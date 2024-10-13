6c34ee945944b54447df8d2ab575006c4be37c9a
gnragazzi <gnragazzi@gmail.com>
2024-10-13 11:12:06 -0300

    Construí acciones, estadoInicial y función reducer de cargar Mantenimiento 2

---
+++ cliente/src/utilidades/reducer_cargar_mantenimiento.js
@@ -0,0 +1,134 @@
+export const acciones_cargar_mantenimiento = {
+  PROXIMA_PANTALLA: 0,
+  PANTALLA_ANTERIOR: 1,
+  CARGAR_LISTA_VEHÍCULOS: 2,
+  CARGAR_LISTA_MECÁNICOS: 3,
+  RESETEAR_CUERPO_MANTENIMIENTO: 4,
+  SELECCIONAR_VEHICULO: 5,
+  SELECCIONAR_MECÁNICO: 6,
+  ES_CAMION: 7,
+  SELECCIONAR_TRABAJO_REALIZADO: 8,
+  SELECCIONAR_COSTO_REPUESTOS: 9,
+  SELECCIONAR_COSTO_MANODEOBRA: 10,
+  SELECCIONAR_KILOMETROS_EN_QUE_SE_REALIZO: 11,
+  SELECCIONAR_FECHA: 12,
+};
+
+export const estadoInicial_cargar_mantenimiento = {
+  cuerpo_cargar_mantenimiento: {
+    trabajos_realizados: "",
+    fecha: "",
+    costo_repuestos: 0,
+    costo_manodeobra: 0,
+    kilometros_en_que_se_realizo: 0,
+    vehiculo: {
+      vehiculoSeleccionado: "",
+      esCamion: "",
+    },
+    mecanicos_seleccionados: [],
+  },
+  lista_vehículos: [],
+  lista_mecánicos: [],
+  pantalla: 0,
+};
+
+export const reducer_cargar_mantenimiento = (estado, accion) => {
+  const { payload } = accion;
+  switch (accion.type) {
+    case acciones_cargar_mantenimiento.PROXIMA_PANTALLA: {
+      return payload.includes(false)
+        ? estado
+        : { ...estado, pantalla: estado.pantalla + 1 };
+    }
+    case acciones_cargar_mantenimiento.PANTALLA_ANTERIOR: {
+      return { ...estado, pantalla: estado.pantalla - 1 };
+    }
+    case acciones_cargar_mantenimiento.CARGAR_LISTA_VEHÍCULOS: {
+      return { ...estado, lista_vehículos: payload };
+    }
+    case acciones_cargar_mantenimiento.CARGAR_LISTA_CHOFER: {
+      return { ...estado, lista_choferes: payload };
+    }
+    case acciones_cargar_mantenimiento.SELECCIONAR_VEHICULO: {
+      const { patente, esCamion } = payload;
+      return estado.cuerpo_cargar_mantenimiento.vehiculo
+        .vehiculoSeleccionado === patente
+        ? {
+            ...estado,
+            cuerpo_cargar_mantenimiento: {
+              ...estado.cuerpo_cargar_mantenimiento,
+              vehiculo: {
+                vehiculoSeleccionado: "",
+              },
+            },
+          }
+        : {
+            ...estado,
+            cuerpo_cargar_mantenimiento: {
+              ...estado.cuerpo_cargar_mantenimiento,
+              vehiculo: {
+                vehiculoSeleccionado: patente,
+                es_camión: esCamion,
+              },
+            },
+          };
+    }
+    case acciones_cargar_mantenimiento.SELECCIONAR_MECÁNICO: {
+      // implementar esta carga (teniendo en cuenta que si el mecánico ya está seleccionado, debería quitarse);
+      const mecánicos =
+        estado.cuerpo_cargar_mantenimiento.mecanicos_seleccionados;
+      mecánicos.includes(payload)
+        ? mecánicos.filter((m) => m != payload)
+        : mecánicos.push(payload);
+      return {
+        ...estado,
+        cuerpo_cargar_mantenimiento: {
+          ...estado.cuerpo_cargar_mantenimiento,
+          mecanicos_seleccionados: mecánicos,
+        },
+      };
+    }
+    case acciones_cargar_mantenimiento.SELECCIONAR_TRABAJO_REALIZADO: {
+      return {
+        ...estado,
+        cuerpo_cargar_mantenimiento: {
+          ...estado.cuerpo_cargar_mantenimiento,
+          trabajos_realizados: payload,
+        },
+      };
+    }
+    case acciones_cargar_mantenimiento.SELECCIONAR_COSTO_REPUESTOS: {
+      return {
+        ...estado,
+        cuerpo_cargar_mantenimiento: {
+          ...estado.cuerpo_cargar_mantenimiento,
+          costo_repuestos: payload,
+        },
+      };
+    }
+    case acciones_cargar_mantenimiento.SELECCIONAR_COSTO_MANODEOBRA: {
+      return {
+        ...estado,
+        cuerpo_cargar_mantenimiento: {
+          ...estado.cuerpo_cargar_mantenimiento,
+          costo_manodeobra: payload,
+        },
+      };
+    }
+    case acciones_cargar_mantenimiento.SELECCIONAR_FECHA: {
+      return {
+        ...estado,
+        cuerpo_cargar_mantenimiento: {
+          ...estado.cuerpo_cargar_mantenimiento,
+          fecha: payload,
+        },
+      };
+    }
+
+    case acciones_cargar_mantenimiento.RESETEAR_CUERPO_MANTENIMIENTO:
+      return estadoInicial_cargar_mantenimiento;
+
+    default:
+      throw new Error(`ERROR: "${accion.type}" no es una acción reconocida...`);
+  }
+};
